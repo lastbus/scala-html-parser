beefdf6daa4a7d61392fccc61fbe82d03bc2c717
lastbus <819307659@qq.com>
2016-03-08 22:03:15 +0800

    对取出的新闻条数进行验证

--- src/main/scala-2.11/com/disanyuzhou/htmlparse/ParserTest.scala
+++ src/main/scala-2.11/com/disanyuzhou/htmlparse/ParserTest.scala
@@ -1,9 +1,12 @@
 package com.disanyuzhou.htmlparse
 
+import com.disanyuzhou.htmlparse.beans.Item
 import com.disanyuzhou.htmlparse.conf.{Command, Query}
 import com.disanyuzhou.htmlparse.format.URLFormat
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
+import scala.reflect.runtime.{universe => ru}
+import scala.tools.asm.Type
 
 /**
  * Created by make on 3/6/16.
@@ -16,24 +19,37 @@ object ParserTest {
    * @param bytes html 二进制流
    * @param conf 解析命令的配置文件，（网页的编码， html 块的提取语句）
    */
-  def parse(bytes: Array[Byte], conf: (String, String, Array[Query])): Unit ={
+  def parse(bytes: Array[Byte], conf: (String, String, Array[Query])): Unit = {
+
     val doc = Jsoup.parse(new String(bytes, conf._2))
 
     for (query <- conf._3) yield {
       val selectedBlock = doc.select(query.queryString)
+      val size = selectedBlock.size
+      val count = query.queryCount
+
+      if(count.contains("-")){
+        val range = count.split("-").map(_.toInt)
+        if(size < range(0) || range(1) < size) throw new Exception(s"except ${selectedBlock.size} ")
+      } else {
+        if(count.toInt != size) throw new Exception("number not equal")
+      }
+
       println(query.queryString)
-      val pickedData = new Array[(String, String)](selectedBlock.size)
+      val pickedData = new Array[(String, String)](size)
+
       val cmdList = query.cmdList
       val className = query.map2Class
       val c = className.getClass.newInstance()
       println(c)
-      println(s"==${selectedBlock.size}==")
-      for (i <- 0 until selectedBlock.size){
+      println(s"==${size}==")
+      for (i <- 0 until size){
 
         cmdList.map(cmd => {
-          val txt = getElementText(selectedBlock.get(i), cmd)
-          if(cmd.format.equals("url-format")) println(URLFormat.format(conf._1, txt))
-          else println(txt)
+          val rawData = getElementText(selectedBlock.get(i), cmd)
+          val output = if(cmd.format.equals("url-format")) URLFormat.format(conf._1, rawData)
+          else rawData
+
         })
 
 //        pickedData(i) = (getElementText(selectedBlock.get(i), cmdList(0)),
@@ -41,7 +57,6 @@ object ParserTest {
       }
 
     }
-    println(count)
   }
 
 
@@ -55,6 +70,8 @@ object ParserTest {
       throw new Exception("wrong command to extract data from html.")
     }
   }
+
+
 
 
 }
